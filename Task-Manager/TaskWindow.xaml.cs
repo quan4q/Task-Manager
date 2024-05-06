9319775ff32d318dfda41dc877061d544bb5f9fe
Alexander Lebedev <itskilobyte@gmail.com>
2024-05-06 20:36:22 +0500

    Created markup for TaskWindow
    Added a window for creating/editing a task. Now Extended.Wpf.Toolkit is also used in the project

--- Task-Manager/TaskWindow.xaml.cs
+++ Task-Manager/TaskWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Task_Manager
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для TaskWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TaskWindow : Window
     {
-        public MainWindow()
+        public TaskWindow()
         {
             InitializeComponent();
-
-            /* Для теста окна
-            Window TaskWindow = new TaskWindow();
-            TaskWindow.Show();
-            */
         }
     }
 }
