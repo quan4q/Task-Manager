470e631d4d1a322b3da640e41d83ad9a0e148d97
Alexander Lebedev <itskilobyte@gmail.com>
2024-05-10 17:27:58 +0500

    Added ListView for Task
    Added the first version of displaying tasks collection in the list.

--- Task-Manager/MainWindow.xaml.cs
+++ Task-Manager/MainWindow.xaml.cs
@@ -22,16 +22,48 @@ namespace Task_Manager
         {
             InitializeComponent();
 
-            /* Для теста окна
-            Window TaskWindow = new TaskWindow();
-            TaskWindow.Show();
-            */
+            DataContext = this; // Для подключения Tasks.
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            //Тут будет заполнение ListView
-        
+            // Парочка тестовых задач для проверки отображения и сортировки
+            Task task1 = new()
+            {
+                Id = 1,
+                Title = "Сходить на пары",
+                Description = "В 18:00 пара по программированию, нельзя пропускать!",
+                Deadline = new DateTime(2024, 05, 10, 18, 00, 00),
+                Category = "Учёба",
+                Priority = 2,
+                IsCompleted = false
+            };
+
+            Task task2 = new()
+            {
+                Id = 2,
+                Title = "Обсуждение проекта",
+                Description = "Нужно встретиться в коворгинге на первом этаже и обсудить детали нашего проекта. ВАЖНО: сортировка, уведомления.",
+                Deadline = new DateTime(2024, 05, 5, 17, 00, 00),
+                Category = "Работа",
+                Priority = 2,
+                IsCompleted = false
+            };
+
+            Task task3 = new()
+            {
+                Id = 3,
+                Title = "Приготовить еду на завтра",
+                Description = "Нашёл в интернете рецепт лазаньи. Говорят вкусная, надо попробовать её приготовить.",
+                Deadline = new DateTime(2024, 05, 6, 21, 30, 00),
+                Category = "Дом",
+                Priority = 1,
+                IsCompleted = false
+            };
+
+            Tasks.Add(task1);
+            Tasks.Add(task2);
+            Tasks.Add(task3);
         }
 
         private void AddTaskButton_Click(object sender, RoutedEventArgs e)
